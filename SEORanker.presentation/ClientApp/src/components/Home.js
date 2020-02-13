69bcb77c120e6b91226f34df4d54d3db7ef55de1
Jeffrey Huang <jeffvh@outlook.com>
2020-02-13 12:16:49 +1100

    create form UI

--- SEORanker.presentation/ClientApp/src/components/Home.js
+++ SEORanker.presentation/ClientApp/src/components/Home.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
+
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const data = new FormData(event.target);
+        console.log(data);
+        fetch('https://localhost:44389/api/rank', {
+            method: 'POST',
+            body: data,
+        });
+    }
 
   render () {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
+      return (
+          <div>
+              <h2>SEO Ranker</h2>
+              <Form onSubmit={this.handleSubmit}>
+                  <FormGroup>
+                      <Label for="search">Search</Label>
+                        <Input id="search" name="search" type="text" placeholder="What do you want to search in Google" />
+                  </FormGroup>
+                  <FormGroup>
+                      <Label for="rank-url">Company Url</Label>
+                      <Input id="rank-url" name="rank-url" type="text" placeholder="What url do you want to rank for?" />
+                  </FormGroup>
+                  <Button type="submit">Submit</Button>
+              </Form>
+          </div>
     );
   }
 }
